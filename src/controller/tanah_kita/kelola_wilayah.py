f0e937102f281ce79d22ae86a133406070d75741
galih752 <rd.galih752@gmail.com>
2024-10-16 10:21:26 +0700

    Updating Tanah Kita

--- src/controller/tanah_kita/kelola_wilayah.py
+++ src/controller/tanah_kita/kelola_wilayah.py
@@ -66,8 +66,8 @@ class KelolaWilayah:
         cleaned_data['profile'] = profile
         tahapan = tahapnya
 
-        file_name = f"{cleaned_data.get('provinsi', None)}_{cleaned_data.get('kabupaten', None)}_{cleaned_data.get('kecamatan', None)}_{cleaned_data.get('desa', None)}_{cleaned_data.get('jenis_wilayah_kelola', None)}_{cleaned_data.get('tahapan', None)}_{tahun}"
-        path_file = f's3://ai-pipeline-raw-data/data/data_descriptive/tanahkita/wilayah_kelola/{tahapan.replace(" ", "_").lower()}/json/{file_name.replace(" ", "_").replace("/", "_").lower()}.json'
+        file_name = f"{cleaned_data.get('provinsi', None)}_{cleaned_data.get('kabupaten', None)}_{cleaned_data.get('kecamatan', None)}_{cleaned_data.get('desa', None)}_{cleaned_data.get('jenis_wilayah_kelola', None)}_{cleaned_data.get('tahapan', None)}_{latitude.replace('.','')}{longitude.replace('.','')}_{tahun}"
+        path_file = f's3://ai-pipeline-raw-data/data/data_descriptive/tanahkita/wilayah_kelola/test/{tahapan.replace(" ", "_").lower()}/json/{file_name.replace(" ", "_").replace("/", "_").lower()}.json'
         metadata = {
             "link": url,
             "tags": [
